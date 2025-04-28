0c903e81a2fefa334d2a2d5b1964b39a26493a5f
Azheem Rahman <azheem.rahman@gmail.com>
2025-04-29 00:35:52 +0800

    add authenticateUser lambda and update serverless.yml

---
+++ backend/src/functions/authenticateUser.ts
@@ -0,0 +1,36 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { errorResponse, successResponse } from "../utils/response";
+import { pool } from "../lib/db";
+
+export const authenticateUserHandler: APIGatewayProxyHandler = async (
+  event
+) => {
+  try {
+    const body = JSON.parse(event.body || "{}");
+    const divisionPassId = body.division_pass_id;
+
+    if (!divisionPassId) {
+      return errorResponse(400, "Bad Request", "division_pass_id is required");
+    }
+
+    const result = await pool.query(
+      `SELECT user_id, role FROM users WHERE division_pass_id = $1`,
+      [divisionPassId]
+    );
+
+    if (result.rowCount === 0) {
+      return errorResponse(404, "Not Found", "Invalid Division Pass");
+    }
+
+    const user = result.rows[0];
+
+    return successResponse({
+      user_id: user.user_id,
+      role: user.role,
+    });
+  } catch (error) {
+    console.error("Error in authenticateUserHandler: ", error);
+
+    return errorResponse(500, "Internal Server Error", "Something went wrong");
+  }
+};
