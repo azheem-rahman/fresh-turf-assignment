2ad87cb9ccf68cc7cec23a9ac114a0901d0d426e
Azheem Rahman <azheem.rahman@gmail.com>
2025-05-03 18:33:43 +0800

    refactor uploadToS3 util function to support separate folders for photos and signatures

--- backend/src/utils/uploadToS3.ts
+++ backend/src/utils/uploadToS3.ts
@@ -3,18 +3,19 @@ import { s3 } from "../lib/s3";
 
 export const uploadToS3 = async (
   base64File: string,
-  photoId: string,
-  type: string
-) => {
+  fileId: string,
+  type: "front" | "back" | "signature"
+): Promise<string> => {
   try {
     const buffer = Buffer.from(base64File, "base64");
 
+    const folder = type === "signature" ? "signatures" : "photos";
     const bucketName = process.env.S3_BUCKET_NAME || "fresh_turf_bucket";
-    const fileName = `${photoId}_${type}.jpg`;
+    const fileName = `${fileId}_${type}.jpg`;
 
     const params: AWS.S3.PutObjectRequest = {
       Bucket: bucketName,
-      Key: `photos/${fileName}`,
+      Key: `${folder}/${fileName}`,
       Body: buffer,
       ContentType: "image/jpeg",
       ACL: "private",
